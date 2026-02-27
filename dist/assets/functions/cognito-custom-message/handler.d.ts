d6b6ee23f380365c0161602cba1b48dead0de256
Paul Smiechowicz <paul@designofadecade.com>
2026-02-27 16:58:21 -0500

    feat: enhance Cognito custom message handler to support MFA and use HTML templates for email messages

--- dist/assets/functions/cognito-custom-message/handler.d.ts
+++ dist/assets/functions/cognito-custom-message/handler.d.ts
@@ -1,6 +1,6 @@
 import type { CustomMessageTriggerHandler } from 'aws-lambda';
 /**
  * Lambda handler for Cognito custom message trigger
- * Customizes the forgot password email with HTML template
+ * Customizes email messages for forgot password and MFA with HTML templates
  */
 export declare const handler: CustomMessageTriggerHandler;
